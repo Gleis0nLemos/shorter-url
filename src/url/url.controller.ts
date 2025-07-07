c5ce3951b6cefd76521f8696c49862338a917df4
Gleison Lemos <gleison04lemos@gmail.com>
2025-07-07 12:48:36 -0300

    feat: add API endpoints for URL shortening and redirection

---
+++ src/url/url.controller.ts
@@ -0,0 +1,32 @@
+import { 
+    Body, 
+    Controller, 
+    Get, 
+    NotFoundException, 
+    Param, 
+    Post, 
+    Res 
+} from "@nestjs/common";
+import { UrlService } from "./url.service";
+import { CreateUrlDto } from "./dto/create-url.dto";
+import { Response } from "express";
+
+
+@Controller()
+export class UrlController {
+    constructor(private urlService: UrlService) {}
+
+    @Post('shorten')
+    async create(@Body() body: CreateUrlDto) {
+        return this.urlService.createUrl(body);
+    }
+
+    @Get(':code')
+    async redirect(@Param('code') code: string, @Res() res: Response) {
+        const destination = await this.urlService.redirect(code);
+
+        if (!destination) throw new NotFoundException();
+
+        return res.redirect(destination)
+    }
+}
