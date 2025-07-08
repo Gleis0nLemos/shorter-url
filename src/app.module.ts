daa755cd0b136b86c458ffce7c19fe947dcd42cb
Gleison Lemos <gleison04lemos@gmail.com>
2025-07-08 17:19:57 -0300

    fix: import auth module to app mudule

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { UrlModule } from './url/url.module';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [PrismaModule, UrlModule],
+  imports: [
+    PrismaModule, 
+    UrlModule, 
+    AuthModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
