cbe9fb0d1b9c8b86a79258d3771181ace7e33bbc
Gleison Lemos <gleison04lemos@gmail.com>
2025-07-07 21:17:56 -0300

    feat: implement JWT auth strategy

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -1,3 +1,20 @@
-export class JwtStrategy {
-    //
+import { Injectable } from "@nestjs/common";
+import { PassportStrategy } from "@nestjs/passport";
+import { ExtractJwt, Strategy } from "passport-jwt"; 
+import { ConfigService } from "@nestjs/config"; 
+
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+    constructor(config: ConfigService) {
+        super({
+            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(), // extracts JWT from the Authorization header
+            ignoreExpiration: false, // do not ignore expiration
+            secretOrKey: config.get<string>('JWT_SECRET'), // JWT_SECRET iin .env
+        });
+    }
+
+    async validate(payload: { sub: string; email: string }) {
+        return { id: payload.sub, email: payload.email }; 
+    }
 }
