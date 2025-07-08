21838a630c28702c8c5cf59ba3e4aef6e2f46de6
Gleison Lemos <gleison04lemos@gmail.com>
2025-07-08 12:47:31 -0300

    feat: add optional decorator to shorten route and AuthGuard to urls/my to protect endpoints

--- src/url/url.controller.ts
+++ src/url/url.controller.ts
@@ -5,11 +5,15 @@ import {
     NotFoundException, 
     Param, 
     Post, 
-    Res 
+    Res, 
+    UseGuards
 } from "@nestjs/common";
 import { UrlService } from "./url.service";
 import { CreateUrlDto } from "./dto/create-url.dto";
 import { Response } from "express";
+import { CurrentUser } from "src/auth/decorators/current-user.decorator";
+import { User } from "@prisma/client";
+import { JwtAuthGuard } from "src/auth/guards/jwt-auth.guard";
 
 
 @Controller()
@@ -17,8 +21,11 @@ export class UrlController {
     constructor(private urlService: UrlService) {}
 
     @Post('shorten')
-    create(@Body() createUrlDto: CreateUrlDto) {
-    console.log(createUrlDto);
+    create(
+        @Body() createUrlDto: CreateUrlDto,
+        @CurrentUser() user?: User, // optional user for authenticated requests
+    ) {
+    // console.log(createUrlDto);
     return this.urlService.createUrl(createUrlDto);
     }
 
@@ -29,5 +36,12 @@ export class UrlController {
         if (!destination) throw new NotFoundException();
 
         return res.redirect(destination)
+    }
+
+    // Protected route: List all URLs for the authenticated user
+    @UseGuards(JwtAuthGuard)
+    @Get('urls/my')
+    findMyUrls(@CurrentUser() user: User) {
+        return this.urlService.findByUserId(user.id);
     }
 }
